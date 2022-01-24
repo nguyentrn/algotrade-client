90ae19be14c948991db8e01b914851188b65d978
Trần Nguyên <nguyentran0113@gmail.com>
2022-01-25 00:25:47 +0700

    remove cookies args

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -103,17 +103,17 @@ export default NextAuth({
     colorScheme: 'light',
   },
 
-  // cookies: {
-  //   sessionToken: {
-  //     name: `next-auth.session-token`,
-  //     options: {
-  //       httpOnly: false,
-  //       sameSite: 'lax',
-  //       path: '/',
-  //       secure: true,
-  //     },
-  //   },
-  // },
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: false,
+      },
+    },
+  },
 
   // Enable debug messages in the console if you are having problems
   debug: false,
